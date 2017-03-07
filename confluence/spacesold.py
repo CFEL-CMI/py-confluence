a558d692ad6cc67fc74e6718627396d3a9b7e9a7
Alexander Franke <alexander.franke@desy.de>
2017-03-07 14:15:39 +0100

    changed to not-deprecated api whereever possible, fixed CCT-144

--- confluence/spacesold.py
+++ confluence/spacesold.py
@@ -1,4 +1,11 @@
-import requests, json
+# -*- coding: utf-8; fill-column: 120 -*-
+#
+# Copyright (C) 2016 Alexander Franke
+
+"""
+This file contains the libraries needed for authentification and creating a new CMI space.
+"""
+
 import os
 import sys
 import string
@@ -6,43 +13,42 @@ import getpass
 import xmlrpc
 import xmlrpc.client
 
+# https://developer.atlassian.com/confdev/confluence-rest-api/confluence-xml-rpc-and-soap-apis/remote-confluence-methods
 
-def auth(user,srv,pwd):
+def auth(user,srv):
 	"""Try to logout from server in case any previous connection is still open. Finally login and return authentification token."""
 	try:
 		srv.confluence2.logout(token)
 	finally:
+		print ("Please enter your password  (user:" + user+")")
+		pwd = getpass.getpass()
 		return srv.confluence2.login(user, pwd)
 
 def xmlrpcServer(serverurl):
 	return xmlrpc.client.ServerProxy(serverurl+'rpc/xmlrpc')
 
-def printResponse(r):
-	print ('{} {}\n'.format(json.dumps(r.json(), sort_keys=True, indent=4, separators=(',', ': ')), r))
-
 def addReadPermissions(token,srv,entity,spacekey):
 	"""Adds the permission VIEWSPACE to a given entity (String of a confluence group or user)"""
 	srv.confluence2.addPermissionToSpace(token,"VIEWSPACE",entity,spacekey)
+
 def addWritePermissions(token,srv,entity,spacekey):
 	"""Adds the permissions VIEWSPACE,EDITSPACE,COMMENT,CREATEATTACHMENT,EDITBLOG to a given entity (String of a confluence group or user)"""
 	permissions = ['VIEWSPACE','EDITSPACE','COMMENT','CREATEATTACHMENT','EDITBLOG']
 	for perm in permissions:
 		srv.confluence2.addPermissionToSpace(token,perm,entity,spacekey)
+
 def addAdminPermissions(token,srv,entity,spacekey):
 	"""Adds the administration permission to a given entity (String of a confluence group or user)"""
 	permissions = ['VIEWSPACE','SETSPACEPERMISSIONS','EDITSPACE','COMMENT','REMOVECOMMENT','CREATEATTACHMENT','REMOVEATTACHMENT','EDITBLOG','REMOVEPAGE','REMOVEBLOG','EXPORTSPACE','SETPAGEPERMISSIONS']
 	for perm in permissions:
 		srv.confluence2.addPermissionToSpace(token,perm,entity,spacekey)
+
 def createCMISpace(serverurl, user, spacekey, spacename, reads,writes,admins):
 	"""Creates a new space (required spacekey, name), sets the homepage according to the CMI template and sets space categories **cfel-cmi** and **cmi-elog-calendar**"""
 	
 	# Get Authentification Token
-	print ("Please enter your password  (user:" + user+")")
-	pwd = getpass.getpass()
-
 	srv     = xmlrpcServer(serverurl)
-	token   = auth(user,srv,pwd)
-
+	token   = auth(user,srv)
 
 	# Content for homepage
 	content = '<ac:layout><ac:layout-section ac:type="two_equal"><ac:layout-cell><ac:structured-macro ac:name="info"><ac:parameter ac:name="title">Overview</ac:parameter><ac:rich-text-body><p>This is the Confluence Space (Wiki, logbook, etc.) of the project '+spacename+'.</p></ac:rich-text-body></ac:structured-macro></ac:layout-cell>'
@@ -55,19 +61,10 @@ def createCMISpace(serverurl, user, spacekey, spacename, reads,writes,admins):
 	content +='<ac:layout-section ac:type="single"><ac:layout-cell><h2>Incomplete tasks</h2><p><ac:structured-macro ac:name="tasks-report-macro"><ac:parameter ac:name="spaces">'+spacekey+'</ac:parameter><ac:parameter ac:name="spaceAndPage">space:'+spacekey+'</ac:parameter><ac:parameter ac:name="pageSize">40</ac:parameter></ac:structured-macro></p></ac:layout-cell></ac:layout-section></ac:layout>'
 	
 	# Create new space
-	print ('----------------------------------------------------------------')
-	print ('                              ')
-	print ('Creating new space')
+	newspace = {'name':spacename,'type': 'global', 'key': spacekey}
+	if srv.confluence2.addSpace(token,newspace):
+		print('Successfully added Space with key '+spacekey)
 
-
-	r = requests.post(serverurl+'rest/api/space',
-		data=json.dumps({'key' : spacekey, 'name':spacename,"description": {"plain": {"value": "CMI space","representation": "plain" }},"metadata": {}}),
-		auth=(user, pwd),
-		headers=({'Content-Type':'application/json'}))
-	printResponse(r)
-	print ('                              ')
-	print ('                              ')
-	print ('----------------------------------------------------------------')
 
 	# Add labels (categories)
 	srv.confluence2.addLabelByNameToSpace(token,"team:cfel-cmi",spacekey)
@@ -78,43 +75,36 @@ def createCMISpace(serverurl, user, spacekey, spacename, reads,writes,admins):
 
 	# Get content ID of homepage of the space
 	homepageid = newspace["homePage"]
-	print(homepageid)
-	r = requests.put(serverurl+'rest/api/content/'+homepageid,
-		data=json.dumps({ "version": {"number": 2 },"title":spacename+ " Home", "type": "page","body": {"storage": {"value": content,"representation": "storage" }}}),
-		auth=(user, pwd),
-		headers=({'Content-Type':'application/json'}))
-	#print(str(r))
-	#printResponse(r)
-	
 
-	#add link to space overview page
-	print ('------------------------------')
-	print ('                              ')
-	print ('    Updating Spaceoverview    ')
+	# Get Page object for homePage
+	homepage = srv.confluence2.getPage(token, homepageid)
 
-	r = requests.get(serverurl+'rest/api/content/14421128',
-		params={'expand' : 'body.storage,version'},
-		auth=(user, pwd))
-	content = r.json()['body']['storage']['value']
-	versionnumber = int(r.json()['version']['number']) + 1
-	print("version number:")
-	print (versionnumber)
-	r = requests.put(serverurl+'rest/api/content/14421128',
-		data=json.dumps({'type':'page', "version": {"number": str(versionnumber)}, "title":"Spaces/Projects/Stashs with relevance to CMI", 'body':{'storage':{'value':content[:-49]+'<ul><li><a href="/display/'+spacekey+'">'+spacename+'</a></li></ul></ac:layout-cell></ac:layout-section></ac:layout>','representation':'storage'}}}),
-		auth=(user, pwd),
-		headers=({'Content-Type':'application/json'}))
-	print ('                              ')
-	print ('           Response:          ')
-	print (r.json())
-	print ('                              ')
-	print ('------------------------------')
-	
+	# Set new content according to tempate defined above
+	homepage["content"] = content
 
+	# Upload content to server
+	if srv.confluence2.updatePage(token, homepage,{"versionComment": "updated Page according to template"}):
+		print('Successfully updated Homepage')
+	else:
+		print('ERROR:Exception raised while trying to upload new content for homepage.')
 
 	# Add permissions
+
 	for entity in reads:
 			addReadPermissions(token,srv,entity,spacekey) 
 	for entity in writes:
 			addWritePermissions(token,srv,entity,spacekey) 
 	for entity in admins:
 			addAdminPermissions(token,srv,entity,spacekey) 
+	
+	#add link to space overview page
+	spaceoverview = srv.confluence2.getPage(token,"14421128")
+	content = spaceoverview["content"] 
+	spaceoverview["content"] = content[:-49]+'<ul><li><a href="/display/'+spacekey+'">'+spacename+'</a></li></ul></ac:layout-cell></ac:layout-section></ac:layout>'
+	# Upload content to server
+	if srv.confluence2.updatePage(token, spaceoverview,{"versionComment": "added link to newly created space (unsorted)"}):
+		print('Successfully updated space overview page')
+	else:
+		print('ERROR:Exception raised while trying to upload new content for space overview page (id 14421128).')
+
+
