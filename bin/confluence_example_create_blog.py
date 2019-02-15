b1d8e45f7e8226a6838af61fb55ad071b96612e0
Alexander Franke <alexander@franke-email.de>
2019-02-15 17:12:26 +0100

    added example for matplotlib as attachment

--- bin/confluence_example_create_blog.py
+++ bin/confluence_example_create_blog.py
@@ -4,6 +4,7 @@
 # Copyright (C) 2018 Alexander Franke
 
 from confluence import *
+from matplotlib import pyplot as plt
 
 # create a new blog post
 confluence = Confluence("USERNAME", "PASSWORD")
@@ -14,7 +15,6 @@ blog.attachments = "/tmp/untitled.html"
 blog.spacekey = "SPACEKEY"
 blog.date = "2019-01-01"
 blog.publish()
-
 
 # get a page from the server (known content_id) and modify it. In this example labels are added.
 # Please note the usage of update() and not publish()
@@ -31,3 +31,15 @@ blogfromeml = Blogpost(confluence)
 blogfromeml.read_eml("pathToEmlFile")
 blogfromeml.spacekey = "SPACEKEY"
 blogfromeml.publish()
+
+# save a matplotlib figure as attachment
+blog_with_plot = Blogpost(confluence)
+plot_name = "PLOTNAME"
+buffer = BytesIO(file_name=plot_name + '.pdf')
+plt.plot()
+# ...
+plt.savefig(buffer, type="pdf")
+blog_with_plot.attachments = [buffer]
+blog_with_plot.title = plot_name
+blog_with_plot.spacekey = "SPACEKEY"
+blog_with_plot.publish()
